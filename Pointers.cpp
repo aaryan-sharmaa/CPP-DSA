68736c490a54bc4808c83e3f73ff8cbc3d64d6b3
Aryan Sharma <aryansharma24022004@gmail.com>
2024-07-11 13:24:50 +0530

    First Commit

--- Pointers.cpp
+++ Pointers.cpp
@@ -1,17 +1,50 @@
 #include <iostream>
 using namespace std;
+
+// int main()
+// {
+//     int a = 10;
+//     int *ptr = &a;
+//     int **pptr = &ptr;
+
+//     cout << "Address of a is " << ptr << endl;
+//     cout << "Address of ptr is " << pptr << endl;
+//     cout << "value of a is " << *ptr << endl;
+//     cout << "value of ptr is " << *pptr << endl;
+//      *ptr = 20;
+//     cout << a << endl;
+//     return 0;
+// }
+
+// int main(){
+//     int *ptr = NULL;
+//     cout << *ptr << endl;
+//     return 0;
+// }
+
+
+// pass by refernce using pointer
+// void changeA(int *ptr)
+// {
+//     *ptr = 20;
+//     cout << *ptr << "\n";
+// }
+
+// pass by refernce using Refernce variables
+void changeA(int &a)
+{
+    a = 20;
+    cout << a << "\n";
+}
+
 int main()
 {
     int a = 10;
-    int *ptr = &a;
-    int **pptr = &ptr;
-
-
-    cout << "Address of a is " << ptr << endl;
-    cout << "Address of ptr is " << pptr << endl;
-    cout << "value of a is " << *ptr << endl;
-    cout << "value of ptr is " << *pptr << endl;
-     *ptr = 20;
-    cout << a << endl;
+    int &b = a;
+    b = 25;
+    cout << b << "\n";
+    cout << a << "\n";
+    // changeA(&a);
+    //cout << a << "\n"
     return 0;
 }
