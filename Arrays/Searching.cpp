d851c0bcb96f759c700c880fb0b9c778c45cef4e
Aryan Sharma <aryansharma24022004@gmail.com>
2024-07-14 23:23:39 +0530

    Searching

--- Arrays/Searching.cpp
+++ Arrays/Searching.cpp
@@ -56,32 +56,76 @@ using namespace std;
 //     return 0;
 // }
 
-// Reverse an Array without using extra spaces (2 Pointer approach)
+//! Reverse an Array without using extra spaces (2 Pointer approach)
 
-void printArr(int *arr, int n)
+// void printArr(int *arr, int n)
+// {
+//     for (int i = 0; i < n; i++)
+//     {
+//         cout << arr[i] << ",";
+//     }
+//     cout << endl;
+// }
+
+// int main()
+// {
+//     int arr[] = {5, 4, 3, 9, 2};
+//     int n = sizeof(arr) / sizeof(int);
+//     int start = 0, end = n - 1;
+//     while (start < end)
+//     {
+//         // int temp = arr[start];
+//         // arr[start] = arr[end];
+//         // arr[end] = temp;
+
+//         // OR
+
+//         swap(arr[start], arr[end]);
+
+//         start++;
+//         end--;
+//     }
+
+//     printArr(arr, n);
+//     return 0;
+// }
+
+// Binary Search - ONLY FOR SORTED ARRAYS
+
+// Step 1 - Find Mid
+// Step 2 - Compare Mid with KEY
+// Step 3 - If KEY is greater than Mid, then search in Right Half
+// Step 4 - If KEY is smaller than Mid, then search in Left Half
+// Step 5 - If KEY is equal to Mid, then return Mid Index
+// Step 6 - If KEY is not present in the array, then return -1
+// Step 7 - If KEY is present in the array, then return MID Index
+
+int binSearch(int *arr, int n, int key)
 {
-    for (int i = 0; i < n; i++)
+    int start = 0, end = n - 1;
+    while (start <= end)
     {
-        cout << arr[i] << " ";
+        int mid = (start + end) / 2;
+        if (arr[mid] == key)
+        {
+            return mid;
+        }
+        else if (arr[mid] > key)
+        {
+            end = mid - 1;
+        }
+        else
+        {
+            start = mid + 1;
+        }
     }
-    cout << endl;
+    return -1;
 }
-
 int main()
 {
-    int arr[] = {5, 4, 3, 9, 2};
+    int arr[] = {2, 4, 6, 8, 10, 12, 14, 16, 18, 20};
     int n = sizeof(arr) / sizeof(int);
-    int start = 0, end = n - 1;
-    while (start < end)
-    {
-        int temp = arr[start];
-        arr[start] = arr[end];
-        arr[end] = temp;
-
-        start++;
-        end--;
-    }
-
-    printArr(arr, n);
+    int key = 16;
+    cout << binSearch(arr, n, key);
     return 0;
 }
